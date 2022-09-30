fccd2fbf14fe1d4ef827abae7f2373b0154d25d2
jinchaeyeon <jinchaeyeon@naver.com>
2022-09-30 18:22:02 +0900

    수정본

--- src/UI/pages/ExperimentsMachinePage/ExperimentsMachinePage.js
+++ src/UI/pages/ExperimentsMachinePage/ExperimentsMachinePage.js
@@ -26,6 +26,7 @@ function ExperimentsMachinePage() {
     setData([
       { t, B3_5_EEG1, B6_8_EEG2, B9_11_PPG_avg, B27_28_X, B29_30_Y, B31_32_Z },
     ]);
+    console.log(data[0].t)
     setMachine(bluetoothService);
   };
 
@@ -44,7 +45,7 @@ function ExperimentsMachinePage() {
         </>
       ) : (
         <Box style={{ backgroundColor: "#191919", padding: "1.rem 2rem", height: "auto" }}>
-          <ExperimentMachineListPageHeader data={data[0].t} machine={machine} propFunction={hightFunction2}/>
+          <ExperimentMachineListPageHeader data={data.t} machine={machine} propFunction={hightFunction2}/>
           <ExperimentMachineListPageMiddle data={data} state={mornitoringState}/>
           <ExperimentMachinePageFooter />
         </Box>
